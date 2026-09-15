c6266f42d74b459c695eed36995384a657f23200
asgah960-crypto <asgah960@gmail.com>
2026-09-16 02:25:40 +0800

    Add background PNG to section below hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,19 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <BenefitsSection />
-      <CategorySection />
-      <PromoBanner />
+      <div className="relative">
+        <div
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage: "url(/section.png)",
+          }}
+        />
+        <div className="relative z-10">
+          <BenefitsSection />
+          <CategorySection />
+          <PromoBanner />
+        </div>
+      </div>
       <ProductGrid />
     </>
   );
